1b5f53e5204327123a03844ef97f8cbb94e1e024
InfantAjayVenus <infantajayv@duck.com>
2026-04-12 12:22:23 +0530

    Normalize SDDM theme install permissions

--- .config/sddm/install.sh
+++ .config/sddm/install.sh
@@ -10,3 +10,6 @@ sudo install -m 0644 "${src}/conf.d/20-session.conf" /etc/sddm.conf.d/20-session
 
 sudo install -d "/usr/share/sddm/themes/${theme}"
 sudo cp -a "${src}/themes/${theme}/." "/usr/share/sddm/themes/${theme}/"
+sudo chown -R root:root "/usr/share/sddm/themes/${theme}"
+sudo find "/usr/share/sddm/themes/${theme}" -type d -exec chmod 0755 {} +
+sudo find "/usr/share/sddm/themes/${theme}" -type f -exec chmod 0644 {} +
